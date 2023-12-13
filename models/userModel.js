5fafa253ffe9bf92855e92264567f7c1a3896e6f
Percy237 <percylarson17@gmail.com>
2023-12-13 14:51:28 +0100

    Initial commit

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    dateOfBirth: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    numChildrenUnder21: {
+      type: Number,
+    },
+    numChildrenAbove21: {
+      type: Number,
+    },
+    placeOfBirth: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model("user", userSchema);
